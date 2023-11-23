603658a30994d499f90f32ba5ac8e4f51b7ddaf7
7Haya <hayafahad46@gmail.com>
2023-11-23 16:21:18 +0300

    Initial Commit

---
+++ MC2/MC2App.swift
@@ -0,0 +1,17 @@
+//
+//  MC2App.swift
+//  MC2
+//
+//  Created by Haya Fahad Alsabr on 09/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
